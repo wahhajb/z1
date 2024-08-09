32e939b02d9275ce1b334898fa3806746f75679a
wahhajb <164786203+wahhajb@users.noreply.github.com>
2024-08-10 00:41:09 +0300

    Update لينك.js

--- plugins/لينك.js
+++ plugins/لينك.js
@@ -3,14 +3,14 @@ const handler = async (m, {conn, args}) => {
   const group = m.chat;
   conn.reply(m.chat, 'https://chat.whatsapp.com/' + await conn.groupInviteCode(group), m, {
     contextInfo: {externalAdReply: {mediaUrl: null, mediaType: 1, description: null,
-      title: 'لينك الجروب',
-      body: 'VENOM 𝐵𝛩𝑇',
+      title: 'رابط القروب',
+      body: 'ستيفن 𝐵𝛩𝑇',
       previewType: 0, thumbnail: fs.readFileSync('./Menu.jpg'),
-      sourceUrl: `https://chat.whatsapp.com/Bu7cwDjLYwLJ93yyUD1tE1`}}});
+      sourceUrl: `https://chat.whatsapp.com/JRz2Z2IqhfbI10PbPE5Ydq`}}});
 };
 handler.help = ['linkgroup'];
 handler.tags = ['group'];
-handler.command = /^لينك|link(gro?up)?$/i;
+handler.command = /^الرابط|link(gro?up)?$/i;
 handler.group = true;
 handler.botAdmin = true;
 export default handler;
