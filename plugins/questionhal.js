301cb2e2c4dceecb659505576ddb6ca577100932
wahhajb <164786203+wahhajb@users.noreply.github.com>
2024-08-10 00:24:56 +0300

    Update questionhal.js

--- plugins/questionhal.js
+++ plugins/questionhal.js
@@ -14,7 +14,7 @@ handler.before = async function(m) {
       clearTimeout(this.tekateki[id][3]);
       delete this.tekateki[id];
     } else if (similarity(m.text.toLowerCase(), json.response.toLowerCase().trim()) >= threshold) m.reply(`*اقــتــربــت مــن الاجــابــة!*`);
-    else m.reply('*❐┃اجـابـة خـاطـئـة يــا بــاكــا┃❌ ❯*');
+    else m.reply('*❐┃اجـابـة خـاطـئـة يــا غبـي┃❌ ❯*');
   }
   return !0;
 };
