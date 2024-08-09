978f3acedb82a9f1cddbb88f0a802cafa5ac90a1
wahhajb <164786203+wahhajb@users.noreply.github.com>
2024-08-10 00:30:16 +0300

    Update ادخل.js

--- plugins/ادخل.js
+++ plugins/ادخل.js
@@ -5,14 +5,14 @@ const handler = async (m, {conn, text, isMods, isOwner, isPrems}) => {
      enviando = true 
   try {
     const link = text //(m.quoted ? m.quoted.text ? m.quoted.text : text : text) || text;
-    if (!link || !link.match(linkRegex)) throw '*[❗] خطأ حط لينك النقابه الي عايز البوت فيها.*\n\n*—◉ مثل:*\n*◉ .انضم https://chat.whatsapp.com/FwEUGxkvZD85fIIp0gKFC*';
+    if (!link || !link.match(linkRegex)) throw '*[❗] خطأ حط رابط القروب الي تبي البوت فيها.*\n\n*—◉ مثل:*\n*◉ .انضم https://chat.whatsapp.com/JRz2Z2IqhfbI10PbPE5Ydq*';
     const [_, code] = link.match(linkRegex) || [];
     if ( isPrems || isMods || isOwner || m.fromMe) {
       const res = await conn.groupAcceptInvite(code);
-      await conn.sendMessage(m.chat, {text: '*[ ✔️ ] تم الانضمام يا سيدي محمد*'}, {quoted: m})
+      await conn.sendMessage(m.chat, {text: '*[ ✔️ ] تم الانضمام *'}, {quoted: m})
       enviando = false 
     } else {
-      conn.sendMessage(m.chat, {text: '*[❗] تم الارسال الي المطور انتظر حتي يتم الموافقة علي مجموعتك*\n\nهناك احتمالات ان لا يتم الموفقه علي المجموعه\nلقد دفعت الكثير من العملات لكن سوف يتم التحقق الاول من مجموعتك\n*'}, {quoted: m});
+      conn.sendMessage(m.chat, {text: '*[❗] تم الارسال الى المطور انتظر حتي يتم الموافقة على مجموعتك*\n\nهناك احتمالات ان لا يتم الموفقه علي المجموعه\nلقد دفعت الكثير من العملات لكن سوف يتم التحقق الاول من مجموعتك\n*'}, {quoted: m});
       const data = global.owner.filter(([id]) => id)[0];
       const dataArray = Array.isArray(data) ? data : [data];
       for (const entry of dataArray) await conn.sendMessage(entry + '@s.whatsapp.net', {text: '*[❗] الينك مش شغال تأكد منه[❗]*\n\n*—◉ Solicitante:* ' + '@' + m.sender.split('@')[0] + '\n*—◉ Link del grupo:* ' + link, mentions: [m.sender], contextInfo: {forwardingScore: 9999999, isForwarded: true, mentionedJid: [m.sender], "externalAdReply": {"showAdAttribution": true, "containsAutoReply": true, "renderLargerThumbnail": true, "title": global.titulowm2, "containsAutoReply": true, "mediaType": 1, "thumbnail": imagen6, "mediaUrl": `${link}`, "sourceUrl": `${link}`}}}, {quoted: m});
